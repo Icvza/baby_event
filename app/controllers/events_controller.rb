7e2f73b780b9536ba3a01f9e1aa0f63637bc2666
Isaac <devisaacalejandro@gmail.com>
2021-05-09 00:09:33 -0400

    Nested route requirments complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
-  
-
+  before_action :set_event, only: [:update, :destroy, :show]
+  before_action :require_ownership, except: [:edit, :new, :create, :index]
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find_by(params[:id])
+    #@event = Event.find_by(params[:id])
   end
 
   def new 
@@ -29,14 +29,15 @@ class EventsController < ApplicationController
 
   def edit
     if params[:baby_id]
+    require_ownership_for_nested_route
       @event = Event.find(params[:baby_id])
     else
-      @event = Event.find(params[:id])
+      set_event
     end
   end
 
   def update
-    @event = Event.find(params[:id])
+    #@event = Event.find(params[:id])
     if @event.update(event_params)
       redirect_to user_path(@event.user_id)
     else 
@@ -45,17 +46,29 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event = event.find(params[:id])
+    #@event = Event.find(params[:id])
     @event.destroy
     redirect_to signup_path
   end
 
   private
     def set_event
-      @event = Event.find(params[:id])
+      @event = Event.find_by(params[:id])
     end
 
     def event_params
       params.require(:event).permit(:content, :time, :user_id, :baby_id)
     end 
+
+    def require_ownership
+      if set_event.user_id != current_user.id
+        redirect_to user_path(params[:id])
+     end
+    end
+
+    def require_ownership_for_nested_route
+      if current_user.id != params[:id].to_i
+        redirect_to user_path(params[:id])
+      end
+    end
 end
